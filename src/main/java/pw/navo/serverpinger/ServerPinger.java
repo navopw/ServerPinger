7c07c172ccde944ae0ab63ebe74b2ca9be6248e7
Stanislav Hetzel <stanislav.hetzel@outlook.com>
2019-02-15 00:17:01 +0100

    Updated screenshots

--- src/main/java/pw/navo/serverpinger/ServerPinger.java
+++ src/main/java/pw/navo/serverpinger/ServerPinger.java
@@ -89,7 +89,6 @@ public class ServerPinger {
                         }
                     }
                 }
-
             } else {
                 status.setLastStateChangeTimestamp(System.currentTimeMillis());
             }
